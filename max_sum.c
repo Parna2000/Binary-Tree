5dbe511163b5a503520b97ac69a45d174beee86d
Parna2000 <21ms0093@mc.iitism.ac.in>
2021-10-22 20:37:33 +0530

    Maximum sum in bst

---
+++ max_sum.c
@@ -0,0 +1,157 @@
+// find the level thst has maximum sum
+// find the number of leaf nodes the tree
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+struct node *createNode(int data)
+{
+    struct node *root = malloc(sizeof(struct node));
+    if (!root)
+    {
+        return NULL;
+    }
+    root->data = data;
+    root->left = NULL;
+    root->right = NULL;
+    return root;
+}
+struct queue
+{
+    int size;
+    int f;
+    int r;
+    struct node **arr;
+};
+struct queue *createQueue()
+{
+    struct queue *q = malloc(sizeof(struct queue));
+    if (!q)
+    {
+        return NULL;
+    }
+    q->size = 20;
+    q->f = -1;
+    q->r = -1;
+    q->arr = malloc(q->size * sizeof(struct node *));
+    if (!q->arr)
+    {
+        return NULL;
+    }
+    return q;
+}
+int isEmpty(struct queue *q)
+{
+    return (q->f == -1);
+}
+int isFull(struct queue *q)
+{
+    return (q->r + 1) % q->size == q->f;
+}
+void EnQueue(struct queue *q, struct node *root)
+{
+    if (isFull(q))
+    {
+        printf("Queue overflow!!\n");
+        return;
+    }
+    else
+    {
+        q->r = (q->r + 1) % q->size;
+        q->arr[q->r] = root;
+        if (q->f == -1)
+        {
+            q->f = q->r;
+        }
+    }
+}
+struct node *DeQueue(struct queue *q)
+{
+    if (isEmpty(q))
+    {
+        printf("Queue underflow!!\n");
+    }
+    struct node *root = q->arr[q->f];
+    if (q->f == q->r)
+    {
+        q->f = q->r = -1;
+    }
+    else
+    {
+        q->f = (q->f + 1) % q->size;
+    }
+    return root;
+}
+
+int max(struct node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        int maxsum = 0, currentsum = 0, level = 0, maxlevel = 0;
+        struct node *temp;
+        struct queue *q = createQueue();
+        EnQueue(q, root);
+        EnQueue(q, NULL);
+        while (!isEmpty(q))
+        {
+            temp = DeQueue(q);
+            // currentsum = currentsum + temp->data;
+            if (temp == NULL)
+            {
+                if (currentsum > maxsum)
+                {
+                    maxsum = currentsum;
+                    maxlevel = level;
+                }
+                currentsum = 0;
+                if (!isEmpty(q))
+                    EnQueue(q, NULL);
+                level++;
+            }
+            else
+            {
+                currentsum = currentsum + temp->data;
+                if (temp->left)
+                {
+                    EnQueue(q, temp->left);
+                }
+                if (temp->right)
+                {
+                    EnQueue(q, temp->right);
+                }
+            }
+        }
+        return maxlevel;
+    }
+}
+
+int main()
+{
+    // struct node *root = (struct node *)malloc(sizeof(struct node));
+    struct node *root = createNode(23); /*           23          */
+    struct node *r1 = createNode(20);   /*           /\          */
+    struct node *r2 = createNode(25);   /*          20 25        */
+    struct node *r3 = createNode(18);   /*          /\  /\       */
+    struct node *r4 = createNode(21);   /*         18 21 24 26   */
+    struct node *r5 = createNode(24);
+    struct node *r6 = createNode(26);
+    struct node *r7 = createNode(29);
+    root->left = r1;
+    root->right = r2;
+    r1->left = r3;
+    r1->right = r4;
+    r2->left = r5;
+    r2->right = r6;
+    // r3->left = r7;
+    // struct node *temp = deep(root);
+    printf("The level having maximum sum is: %d\n", max(root));
+    return 0;
+}
