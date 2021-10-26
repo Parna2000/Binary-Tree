6984f696928ae9297c9ee3c078b9bf24e4abb4d0
Parna2000 <21ms0093@mc.iitism.ac.in>
2021-10-26 14:05:04 +0530

    Fully balanced tree

---
+++ fully_balanced_tree.c
@@ -0,0 +1,71 @@
+// Check if the given binary tree is balanced or not
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    // int size;
+    struct node *left;
+    struct node *right;
+};
+struct node *createNode(int data)
+{
+    struct node *root = malloc(sizeof(struct node));
+    if (!root)
+    {
+        return NULL;
+    }
+    root->data = data;
+    root->left = NULL;
+    root->right = NULL;
+    return root;
+}
+int height(struct node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        int l = height(root->left);
+        int r = height(root->right);
+        if (l > r)
+        {
+            return l + 1;
+        }
+        else
+        {
+            return r + 1;
+        }
+    }
+}
+int main()
+{
+    // struct node *root = (struct node *)malloc(sizeof(struct node));
+    struct node *root = createNode(23); /*            23          */
+    struct node *r1 = createNode(20);   /*            /\          */
+    struct node *r2 = createNode(25);   /*           20 25        */
+    struct node *r3 = createNode(18);   /*           /\  /\       */
+    struct node *r4 = createNode(21);   /*         18 21 24 26    */
+    struct node *r5 = createNode(24);
+    struct node *r6 = createNode(26);
+    struct node *r7 = createNode(29);
+    root->left = r1;
+    root->right = r2;
+    r1->left = r3;
+    r1->right = r4;
+    r2->left = r5;
+    r2->right = r6;
+    r3->left = r7;
+    if (height(root->right) == height(root->left))
+    {
+        printf("The tree is a fully balanced tree\n");
+    }
+    else
+    {
+        printf("The tree is not a fully balanced tree\n");
+    }
+    return 0;
+}
