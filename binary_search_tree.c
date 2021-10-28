f8e7ea9a3c427825a4e3def02667e396ac40c9b8
Parna2000 <21ms0093@mc.iitism.ac.in>
2021-10-28 21:42:07 +0530

    Binary Search tree

---
+++ binary_search_tree.c
@@ -0,0 +1,70 @@
+// Searching an element in a binary search tree
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+struct node *createNode(int data)
+{
+    struct node *root = malloc(sizeof(struct node));
+    if (!root)
+    {
+        return NULL;
+    }
+    root->data = data;
+    root->left = NULL;
+    root->right = NULL;
+    return root;
+}
+int Search(struct node *root, int data)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    else if (root->data == data)
+    {
+        return 1;
+    }
+    else if (root->data > data)
+    {
+        return Search(root->left, data);
+    }
+    else if (root->data < data)
+    {
+        return Search(root->right, data);
+    }
+}
+int main()
+{
+    // struct node *root = (struct node *)malloc(sizeof(struct node));
+    struct node *root = createNode(23); /*            23          */
+    struct node *r1 = createNode(20);   /*            /\          */
+    struct node *r2 = createNode(25);   /*           20 25        */
+    struct node *r3 = createNode(18);   /*           /\  /\       */
+    struct node *r4 = createNode(21);   /*         18 21 24 26    */
+    struct node *r5 = createNode(24);
+    struct node *r6 = createNode(26);
+    struct node *r7 = createNode(29);
+    root->left = r1;
+    root->right = r2;
+    r1->left = r3;
+    r1->right = r4;
+    r2->left = r5;
+    r2->right = r6;
+    // r3->right = r7;
+    int data;
+    printf("Enter the data:\n");
+    scanf("%d", &data);
+    if (Search(root, data))
+    {
+        printf("This node is present in the given tree\n");
+    }
+    else
+    {
+        printf("This node is not present in the given tree\n");
+    }
+}
