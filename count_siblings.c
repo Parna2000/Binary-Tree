91a17ef7b7555ce25ee3c484b8ebf2695fd0decd
Parna2000 <21ms0093@mc.iitism.ac.in>
2021-10-28 21:38:27 +0530

    Count Siblings

---
+++ count_siblings.c
@@ -0,0 +1,51 @@
+// count number of siblings of a node
+// sum of all the elements of a generic tree
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *firstChild;
+    struct node *nextSibling;
+};
+struct node *createNode(int data)
+{
+    struct node *root = malloc(sizeof(struct node));
+    if (!root)
+    {
+        return NULL;
+    }
+    root->data = data;
+    root->firstChild = NULL;
+    root->nextSibling = NULL;
+    return root;
+}
+// struct node*find(int data){
+// return;
+// }
+int sib(struct node *current)
+{
+    int s = 0;
+    while (current->nextSibling)
+    {
+        s++;
+        current=current->nextSibling;
+    }
+    return s;
+}
+int main()
+{
+    struct node *root = createNode(23);
+    struct node *r1 = createNode(22);
+    struct node *r2 = createNode(24);
+    struct node *r3 = createNode(25);
+    struct node *r4 = createNode(26);
+    struct node *r5 = createNode(27);
+    root->firstChild = r1;
+    r1->nextSibling = r2;
+    r2->nextSibling = r3;
+    r3->nextSibling = r4;
+    r3->firstChild = r5;
+    printf("Number of siblings of %d node is: %d\n", r3->data, sib(r3));
+    return 0;
+}
