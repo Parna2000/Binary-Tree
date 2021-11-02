4ee45090ce073d48c556dde1e026f72c6c596c95
Parna2000 <21ms0093@mc.iitism.ac.in>
2021-11-02 17:41:17 +0530

    New file

--- one.c
+++ one.c
@@ -1,17 +1,15 @@
-// Find an element in the bst closest to the given key
-// Check if the given tree is AVL(Adelson-Velskii-Landis) tree or not
+// size=no. of descendents
 #include <stdio.h>
 #include <stdlib.h>
 struct node
 {
     int data;
-    // int size;
     struct node *left;
     struct node *right;
 };
 struct node *createNode(int data)
 {
-    struct node *root = malloc(sizeof(struct node));
+    struct node *root = (struct node *)malloc(sizeof(struct node));
     if (!root)
     {
         return NULL;
@@ -21,46 +19,126 @@ struct node *createNode(int data)
     root->right = NULL;
     return root;
 }
-int binaryTree(struct node *root)
+struct queue
 {
-    if (root == NULL)
+    int f;
+    int r;
+    int capacity;
+    struct node **arr;
+};
+struct queue *CreateQueue()
+{
+    struct queue *q = malloc(sizeof(struct queue));
+    if (!q)
     {
-        return 1;
+        return NULL;
     }
-    if (root->left != NULL && root->left->data > root->data)
+    q->capacity = 20;
+    q->f = -1;
+    q->r = -1;
+    q->arr = malloc(q->capacity * sizeof(struct node *));
+    if (!q->arr)
     {
-        return 0;
+        return NULL;
     }
-    if (root->right != NULL && root->right->data < root->data)
-    {
-        return 0;
-    }
-    if (!binaryTree(root->left) || !binaryTree(root->right))
-    {
-        return 0;
-    }
-    return 1;
+    return q;
 }
-int height(struct node *root)
+int queueIsEmpty(struct queue *q)
 {
-    if (root == NULL)
+    return q->f == -1;
+}
+int queueIsFull(struct queue *q)
+{
+    return (q->r + 1) % q->capacity == q->f;
+}
+void EnQueue(struct queue *q, struct node *root)
+{
+    if (queueIsFull(q))
     {
-        return 0;
+        printf("Queue overflow!!\n");
+        return;
+    }
+    q->r = (q->r + 1) % q->capacity;
+    q->arr[q->r] = root;
+    if (q->f == -1)
+    {
+        q->f = q->r;
+    }
+}
+struct node *DeQueue(struct queue *q)
+{
+    if (queueIsEmpty(q))
+    {
+        printf("Queue underflow!!\n");
+        return NULL;
+    }
+    struct node *root = q->arr[q->f];
+    if (q->f == q->r)
+    {
+        q->f = q->r = -1;
     }
     else
     {
-        int l = height(root->left);
-        int r = height(root->right);
-        if (l > r)
+        q->f = (q->f + 1) % q->capacity;
+    }
+    return root;
+}
+int maxElement(struct node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    else if (root->left == NULL && root->right == NULL)
+    {
+        return root->data;
+    }
+    else
+    {
+        int max = root->data;
+        int l = maxElement(root->left);
+        int r = maxElement(root->right);
+        if (l > max)
         {
-            return l + 1;
+            max = l;
         }
-        else
+        if (r > max)
         {
-            return r + 1;
+            max = r;
         }
+        return max;
     }
 }
+
+int maxNode(struct node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    struct queue *q = CreateQueue();
+    struct node *temp;
+    int max = root->data;
+    EnQueue(q, root);
+    while (!queueIsEmpty(q))
+    {
+        temp = DeQueue(q);
+        if (temp->data > max)
+        {
+            max = temp->data;
+        }
+        if (temp->left)
+        {
+            EnQueue(q, temp->left);
+        }
+        if (temp->right)
+        {
+            EnQueue(q, temp->right);
+        }
+    }
+    return max;
+}
+
 int main()
 {
     // struct node *root = (struct node *)malloc(sizeof(struct node));
@@ -71,23 +149,15 @@ int main()
     struct node *r4 = createNode(21);   /*         18 21 24 26    */
     struct node *r5 = createNode(24);
     struct node *r6 = createNode(26);
-    struct node *r7 = createNode(17);
-    struct node *r8 = createNode(16);
+    struct node *r7 = createNode(29);
     root->left = r1;
     root->right = r2;
     r1->left = r3;
     r1->right = r4;
     r2->left = r5;
     r2->right = r6;
-    // r3->left = r7;
-    // r7->left = r8;
-    if (binaryTree(root) && ((height(root->left) - height(root->right)) <= 1 && (height(root->left) - height(root->right)) >= -1))
-    {
-        printf("The tree is an AVL tree\n");
-    }
-    else
-    {
-        printf("The tree is not an AVL tree\n");
-    }
+    r3->left = r7;
+    printf("The size of the tree found recursively is: %d\n", maxElement(root));
+    printf("The size of the tree found non-recursively is: %d\n", maxNode(root));
     return 0;
 }
